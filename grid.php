35a4248f82f7c766c72710a5373053aff4dfbdcc
box <104191286+SikorskiMaksymilian@users.noreply.github.com>
2022-12-06 14:01:33 +0100

    Add files via upload

---
+++ grid.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styl2.css" media="all">
+	<title>STRONKA GRID</title>
+</head>
+<body>
+<body>
+    <hr>
+    <div id="man">
+        <div class="baner">
+            <h2>Grid!</h2>
+        </div>
+    </div>
+    
+    <div id="GRIDZIK"><br>
+     	<div class="wrapperek">
+            <div class="boxika">Oto</div>
+            <div class="boxikb">pro</div>
+            <div class="boxikc">sty</div>
+            <div class="boxikd">przy</div>
+            <div class="boxike">kład</div>
+            <div class="boxikf">gri</div>
+            <div class="boxikg">da</div>
+            <div class="boxikh">w</div>
+            <div class="boxiki">html</div>
+        </div>
+        <br><a href="index.php">Powrót do strony głównej</a>
+    </div>
+
+    <div id="podstopka">
+        Stronę wykonał Maksymilian
+    </div>
+
+</body>
+</body>
+</html>
